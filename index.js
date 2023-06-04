e6e43c85130fc0eccfca531b5e02614c4c1c4c1c
La-Ruda <herempen@gmail.com>
2023-06-04 19:21:29 +0200

    podcast test test

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
-const podcast1 = {
-  num: '126',
-  title: 'Robot, který snědl koblihu',
-  guest: 'Radovan Siwek',
+
+console.log("Spirit of this Machine, heed my will!");
+
+
+import { Podcast } from "./Podcast/index.js";
+
+
+
+// const podcast1 = {
+//   num: '126',
+//   title: 'Robot, který snědl koblihu',
+//   guest: 'Radovan Siwek',
+// };
+
+
+const episodes = [
+  {
+    num: 127,
+    title: 'Hledání plyšového Yettiho',
+    guest: 'Vojtěch Ryba',
+  },
+  {
+    num: 128,
+    title: 'Moderní hrachová polévka',
+    guest: 'Kamila Štancová',
+  },
+  {
+    num: 129,
+    title: 'Poloautomatické zrcadlo',
+    guest: 'Janka Janovská',
+  },
+  {
+    num: 130,
+    title: 'Máčené hlavy parlamentu',
+    guest: 'Jonáš Daněk',
+  },
+  {
+    num: 131,
+    title: 'Služby na hraně přívěsu',
+    guest: 'Tereza Křivánková',
+  },
+  {
+    num: 132,
+    title: 'Klasifikace sněžných klokanů',
+    guest: 'Josef Stix',
+  },
+  {
+    num: 133,
+    title: 'Rybolov v Oceánu bouří',
+    guest: 'Ludmila Gajová',
+  },
+];
+
+
+const mapEpisodes = () => {
+
+  const episodeContainer = document.querySelector(".episodes-list");
+
+  episodeContainer.innerHTML = episodes.map(Podcast).join("");
+
 };
 
-console.log('funguju!', podcast1);
+
+mapEpisodes()
+
